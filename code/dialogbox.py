496a9bed7567a77f4c352dd2b3c85e115f88c9c5
Matteo <matteo97melis@gmail.com>
2023-01-08 11:35:13 +0100

    Text features adding

---
+++ code/dialogbox.py
@@ -0,0 +1,52 @@
+import pygame
+from text import testi
+
+
+class MyWindow:
+    def __init__(self, text, font_size=10):
+        # Create the main window
+        self.x = 100
+        self.y = 700
+        self.width = 1150
+        self.height = 75
+        self.text = text
+        self.font = pygame.font.Font("font.ttf", font_size)
+        self.text_surface = self.font.render(self.text, True, (255, 255, 255))
+        self.testi = testi()
+        self.show_dialog_box = False #init dialogbox at False don't show
+        #self.dialog_box = DialogBox(100, 100, 200, 50, self.testo_out)
+       
+        
+
+    def draw(self, screen):
+        # Draw the rectangle
+        rect = pygame.draw.rect(screen, (0, 0, 0), (self.x, self.y, self.width, self.height))
+        # Blit the text onto the screen
+        screen.blit(self.text_surface, (rect.x + 10, rect.y + 10))
+        #rect2 = pygame.draw.rect(screen(255, 255, 255), (self.x, self.y, self.width+10, self.height+10)
+
+
+
+
+    def toggle_dialog_box(self): #sopra inizializzata a False, qui cambia da true a False e viceversa quando chiamata (ossia quando si preme il mouse più giù)
+         # Toggle the value of the show_dialog_box variable
+        #global show_dialog_box
+        self.show_dialog_box = not self.show_dialog_box
+
+    
+    def run_window(self, screen):
+            #get the output text from the method of the output text
+            text = self.testi.dialogues()
+        # Join the wrapped lines of text into a single string, separated by newlines
+            self.testo_out = text
+            self.dialog_box = MyWindow(self.testo_out) 
+            self.dialog_box.draw(screen)
+            
+
+    
+        
+# Create an instance of the MyWindow class
+#window = MyWindow()
+
+# Call the show_window() method to display the window
+#window.run_window(window.dialogues())
---
+++ code/text.py
@@ -0,0 +1,67 @@
+class testi:
+    def __init__(self):
+         #check dialogue line
+        self.squirrel = False
+        self.kraken = False
+        self.skeleton = False
+        self.bamboo = False
+        self.librarian = False
+        self.statue = False
+        ## ALL THE VARIABLES UP HERE MIGHT BE USELESS##
+        #check interaction
+        self.squirrel_check = False
+        self.kraken_check = False
+        self.skeleton_check = False
+        self.bambu_check = False
+        self.statue_check = False
+        self.missions_check = 0
+        self.who_is_talking = 6
+    
+    def dialogues (self): #funzione con tutte le possibilità del gioco e le linee di testo [fare diagramma]
+#Dialogo squirrel        
+        if self.squirrel == False and self.who_is_talking == 1: #if first interaction with squirrel and you are talking with squirrel
+            self.squirrel_check = True #then check that you have talked with squirrel
+            return "SQUIRREL: I'm tired of eating humans! I could use a book for vegan recipe" #and print this
+        elif self.squirrel == True and self.who_is_talking == 1 and self.statue_check == True: #if you already talk with squirrel and you are talking with squirrel and you have interacted with the statute (so you have the book)
+            self.missions_check += 1 #then the mission is complete
+            return "SQUIRREL: Thank you! Now i can finally eat those noisy bamboo" #and print this
+#Dialogo kraken
+        if self.kraken == False and self.who_is_talking == 2: 
+            self.kraken_check = True
+            return "KRAKEN: I'm too old to keep destroying these ships. Bring me a tabloid!"
+        elif self.kraken == True and self.who_is_talking == 2 and self.statue_check == True:
+            self.missions_check += 1
+            return "KRAKEN: Thank you! Finally I can read something about the royal family... What?! The Queen died?!?"
+#Dialogo skeleton
+        if self.skeleton == False and self.who_is_talking == 3: 
+            self.skeleton_check = True
+            return "SKELETON: The boss disappeard! He left this letter but i cannod read it! I need a vocabulary."
+        elif self.skeleton == True and self.who_is_talking == 3 and self.statue_check == True:
+            self.missions_check += 1
+            return "SKELETON: Oh thank you, he just said he needed a holiday"
+#Dialogo Bambu
+        if self.bamboo == False and self.who_is_talking == 4: 
+            self.bambu_check = True
+            return "BAMBOO: My muscle are all sore! I need a book to learn how to strech!"
+        elif self.bamboo == True and self.who_is_talking == 4 and self.statue_check == True:
+            self.missions_check += 1
+            return "BAMBOO: Oh my God, Thank you! I could finally hug my children again!"
+#Dialogo statua
+        if self.statue == False and self.who_is_talking == 5: 
+            return "That's a nice statue"
+        elif self.statue == True and self.who_is_talking == 5:
+            self.statue_check = True  
+            self.squirrel = True
+            self.kraken = True
+            self.bamboo = True
+            self.skeleton = True
+            return "THE FLOOR SHAKES! 3 books appear on the floor"
+#Dialogo libraio
+        if self.librarian == False and self.who_is_talking == 6:
+            return "LIBRARIAN: You!!! You are the hero the king sent! Go talk with the four magic beasts in the corner of the map"
+        elif self.squirrel_check == True and self.kraken_check == True and self.skeleton_check == True and self.bambu_check == True and self.who_is_talking == 6: #if you have talked with all the magic beasts
+            self.librarian = True
+            self.statue = True
+            return "LIBRARIAN: For all this stuff you should pray to that statue! But I have a tabloid! ""YOU HAVE GOT TABLOID”"
+        elif self.librarian == True and self.statue_check == True and self.missions_check == 4 and self.who_is_talking == 6: # check if you have completed all 4 the mission and interacted with the statue
+            return "LIBRARIAN: Thank you! You have saved the Kingdom!"
