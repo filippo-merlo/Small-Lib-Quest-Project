b5ab7a7f8e3b8c8ee8a3476220b7312e6d525632
Filippo <filippo99merlo@gmail.com>
2023-01-10 18:39:35 +0100

    player animations

--- code/player.py
+++ code/player.py
@@ -11,36 +11,57 @@ class Player(pygame.sprite.Sprite): # Inherit Sprite method of sprite class
         self.image = pygame.transform.scale(self.image, (PLAYERSIZE_W, PLAYERSIZE_H)) # scale image
         self.rect = self.image.get_rect(topleft = pos) # create the rect to blit on the screen surf. of the same size of the image and pos given by pos. pos is extracted from the WORLD_MAP in the level class
         self.hitbox = self.rect.inflate(-10,-40)
-        #self.hitbox = pygame.Rect(self.rect.x, self.rect.y, self.rect.w, self.rect.h)
+        self.animation_speed = 0.20
+        self.frame_index = 0
+        self.import_player_assets()
+        self.status = 'right_still'
+
         self.direction = pygame.math.Vector2() # will give as a 2d vector to define the player movement
                                                # now x,y = 0, then we want to modify them such as when you pres right x += 1 * player_speed
         self.speed = SPEED # set pix/sec speed of moovement of the player
         self.obstacle_sprites = obstacle_sprites # instantiate the obstacle_sprites =  pygame.sprite.Group() to use it in this class
 
-    #def import_player_assets(self):
-    #    character_path =  "./sprites/characters"
-    #    self.animations = {}
-    #    for animation in self.animations.keys():
-    #       full_path =  character_path + animation 
-    #       self.animations[animation] = import_folder(full_path)
-    #    print(self.animations)
+    def import_player_assets(self):
+        character_path =  "./sprites/characters/"
+        frames_dict = {'player_still':[],'player_moove':[]}
+        self.animations = {}
+        r = range(1,6)
+        for img in frames_dict.keys():
+            for i in r:
+                full_path =  character_path + img + '_'+str(i)+'.png'
+                frames_dict[img].append(pygame.image.load(full_path))
+        self.animations['right_still'] = [pygame.transform.scale(i, (PLAYERSIZE_W, PLAYERSIZE_H)) for i in frames_dict['player_still']]
+        self.animations['left_still'] = [pygame.transform.scale(pygame.transform.flip(i, flip_x = True, flip_y=False), (PLAYERSIZE_W, PLAYERSIZE_H)) for i in frames_dict['player_still']]
+        self.animations['right'] = [pygame.transform.scale(i, (PLAYERSIZE_W, PLAYERSIZE_H)) for i in frames_dict['player_moove']]
+        self.animations['left'] = [pygame.transform.scale(pygame.transform.flip(i, flip_x = True, flip_y=False), (PLAYERSIZE_W, PLAYERSIZE_H)) for i in frames_dict['player_moove']]
+
+        
 
     ### Connect the user input with variations in the direction 2D  vector
     def input(self):
         keys = pygame.key.get_pressed() # get input
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = self.status.replace('_still','')
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = self.status.replace('_still','')
         else:
             self.direction.y = 0
             
         if keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         elif keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         else:
             self.direction.x = 0
+
+    def get_status(self):
+        if self.direction.y == 0 and self.direction.x == 0:
+            if not '_still' in self.status:
+                self.status = self.status + '_still'
 
     ### Use the direction vector to update the player rect position
     def move(self,speed):
@@ -69,6 +90,19 @@ class Player(pygame.sprite.Sprite): # Inherit Sprite method of sprite class
                     if self.direction.y < 0: # moving up
                         self.hitbox.top = sprite.hitbox.bottom
 
+    def animate(self):
+        animation = self.animations[self.status]
+
+        # loop ove the frame index
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+        # set the image
+        self.image = animation[int(self.frame_index)]
+        self.rect = self.image.get_rect(center = self.hitbox.center)
+
     def update(self):
         self.input()
+        self.get_status()
+        self.animate()
         self.move(self.speed)
