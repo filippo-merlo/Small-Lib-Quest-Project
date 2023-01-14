e5cd93a00952c0d4d7d36436a7f2ad2e14150cc9
Filippo <filippo99merlo@gmail.com>
2023-01-14 22:16:14 +0100

    comm

--- code/settings.py
+++ code/settings.py
@@ -4,9 +4,6 @@ from screeninfo import get_monitors
 monitor_sizes = get_monitors()
 screen_width = monitor_sizes[0].width # screen's width
 screen_height = monitor_sizes[0].height # screen's height
-# Set the game window' size
-#WIDTH = int(screen_width-screen_width*0.2) # to reduce it of 20%
-#HEIGHT = int(screen_height-screen_height*0.2) # to reduce it of 20%
 
 FPS = 60 # set frame rate
 ZOOM = 3 #Parameter to scale the map to the screen
