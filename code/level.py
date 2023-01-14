425d8b2767e12aba522c3afbfae56968e575e3ba
Matteo <matteo97melis@gmail.com>
2023-01-14 11:57:31 +0100

    shsh

--- code/level.py
+++ code/level.py
@@ -44,6 +44,7 @@ class Level:
         self.dialogue_block = True
         self.dialoguebox = DialogueBox() #instance of the class Mywindow
         self.testi = testi() #instance of the class testi
+        self.first_screen = True
         
         
         self.menu = menu()
@@ -328,6 +329,8 @@ class Level:
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit() # quit pygame
                     sys.exit() # quit the while loop
+                if event.key == pygame.K_u:
+                    self.first_screen = False
                 if event.key == pygame.K_SPACE:
                     for name,pos,surf in objects_offset_pos: #check in which rect the player is in by the collision betw
                         width = surf.get_width()+20
@@ -347,7 +350,7 @@ class Level:
         self.update_animated_tiles(self.animations_list, self.player)
         self.update_animated_objects(self.animations_list_objects, self.player)
         self.visible_sprites.update()
-        self.menu.run()  
+        self.menu.run(self.first_screen)  
         
         # Make dialogues work
         self.check_interaction() #run the events interaction function
--- code/start_and_end_menu.py
+++ code/start_and_end_menu.py
@@ -13,7 +13,9 @@ class menu:
         self.half_height = self.display_surface.get_size()[1]//2
         self.font_top_end = pygame.font.Font("./graphics/font/font.ttf", 80) #define font and size
         self.font_bottom_end = pygame.font.Font("./graphics/font/font.ttf", 50)
-    
+        self.image_start = pygame.image.load("./graphics/Title/Small Lib Quest(Gold font).png").convert_alpha()
+        self.font_bottom_start =  pygame.font.Font("./graphics/font/font.ttf", 30)
+
     def blit_text(self,surface, text, pos, font, color=pygame.Color('White')):
         words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
         space = font.size(' ')[0]  # The width of a space.
@@ -31,8 +33,37 @@ class menu:
             x = pos[0]  # Reset the x.
             y += word_height  # Start on new row.
             
-    def start_menu(self):
-            print('')
+    def start_menu(self, first_screen):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_l]: 
+            
+            # Create the rectangles surfaces
+            surface_top = pygame.Surface((self.width, self.half_height))
+            surface_bottom = pygame.Surface((self.width, self.half_height))
+
+            # Fill the rectangles surfaces with the same color
+
+            surface_top.fill((0, 0, 0))
+            surface_bottom.fill((0, 0, 0))
+
+            # Create the rectangles
+            rect_top_rect = surface_top.get_rect()
+            rect_bottom_rect = surface_bottom.get_rect(bottom = self.height)
+
+            # Create the text surface
+            text_bottom = self.font_bottom_start.render("Press arrow keys to move and spacebar to interact!", True, (255, 255, 255))
+
+
+            # Blit the text/image on the center of their corresponding rectangle surfaces
+            surface_top.blit(self.image_start, self.image_start.get_rect(center = surface_top.get_rect().center))
+            surface_bottom.blit(text_bottom,text_bottom.get_rect(center = surface_bottom.get_rect().center))
+            
+
+            # Blit the rectangles surfaces on the main screen
+            if first_screen:
+                self.display_surface.blit(surface_top, rect_top_rect)
+                self.display_surface.blit(surface_bottom, rect_bottom_rect)
+
    
     def end_screen(self):
         #if endgame == True and show_dialog_box == False: #if last interaction True and there is no dialogbox (so you should have close it)
@@ -45,6 +76,7 @@ class menu:
             surface_bottom = pygame.Surface((self.width, self.half_height))
 
             # Fill the rectangles surfaces with the same color
+
             surface_top.fill((0, 0, 0))
             surface_bottom.fill((0, 0, 0))
 
@@ -56,9 +88,11 @@ class menu:
             text_top = self.font_top_end.render("THE END", True, (255, 255, 255))
             text_bottom = self.font_bottom_end.render("Thank you for playing! Press esc to close", True, (255, 255, 255))
 
+
             # Blit the text on the center of their corresponding rectangle surfaces
             surface_top.blit(text_top,text_top.get_rect(center = surface_top.get_rect().center))
             surface_bottom.blit(text_bottom,text_bottom.get_rect(center = surface_bottom.get_rect().center))
+            
 
             # Blit the rectangles surfaces on the main screen
             self.display_surface.blit(surface_top, rect_top_rect)
@@ -66,8 +100,8 @@ class menu:
 
         
 
-    def run(self):
-        self.start_menu()
+    def run(self,first_screen):
+        self.start_menu(first_screen)
         self.end_screen()
 
         
