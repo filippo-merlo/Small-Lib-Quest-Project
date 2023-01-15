b9fe3d50d04bfa60359959b964f8634e8d68ea68
Matteo <matteo97melis@gmail.com>
2023-01-15 15:17:03 +0100

    lol

--- code/start_and_end_menu.py
+++ code/start_and_end_menu.py
@@ -19,7 +19,7 @@ class Menu:
         # Settings for the start sceen
         self.image_start = pygame.image.load(Path("./graphics/Title/Small Lib Quest(Gold font).png")).convert_alpha() #import image for start screen #convert_alpha use only the actual pixel of the png
         self.image_end = pygame.image.load(Path("./graphics/Title/THE_END(Gold font).png")).convert_alpha() #import image for end screen #convert_alpha use only the actual pixel of the png
-        self.font =  pygame.font.Font(Path("./graphics/font/font.ttf"), int(font_multiplier)) #define font and size of the start font
+        self.font =  pygame.font.Font(Path("./graphics/font/font.ttf"), int(0.9*font_multiplier)) #define font and size of the start font
         self.end_font = pygame.font.Font(Path("./graphics/font/font.ttf"), int(1.5*font_multiplier)) #define font and size of the end font
 
     def blit_text(self, surface, text, pos, font, color=pygame.Color('White')):
